cc2fbce1eca31aa5640dc10ef4c3e507ee1abc7c
Joe Burgess <joe@flatironschool.com>
2014-10-13 21:32:59 -0400

    Created member variables

--- ObjectOrientedPeople/app/src/main/java/com/flatironschool/objectorientedpeople/Person.java
+++ ObjectOrientedPeople/app/src/main/java/com/flatironschool/objectorientedpeople/Person.java
@@ -1,9 +1,16 @@
 package com.flatironschool.objectorientedpeople;
 
+import java.util.ArrayList;
+
 /**
  * Created by altyus on 6/27/14.
  */
 public class Person {
 
+    Float mHeight;
+    Integer mAge;
+    String mName;
+    Boolean isFemale;
+    ArrayList<Person>mFriends;
 
 }
